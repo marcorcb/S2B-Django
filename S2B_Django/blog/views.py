e029489978add5ded0c01f966b6c6f2332e6165a
marcorcb <marcorcbraga@gmail.com>
2017-10-04 22:43:10 -0300

    - Class 2 changes

--- S2B_Django/blog/views.py
+++ S2B_Django/blog/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+
+def index(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'index.html', {'posts':posts})
